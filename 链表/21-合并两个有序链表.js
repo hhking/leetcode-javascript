50db62c9ac6667eaba102252e33f2e013ec6cad2
hhking <huhhking@gmail.com>
2020-10-22 20:15:58 +0800

    合并两个有序链表

---
+++ 链表/21-合并两个有序链表.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+// 递归
+var mergeTwoLists = function(l1, l2) {
+  if (!l1) return l2;
+  if (!l2) return l1;
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoLists(l1, l2.next);
+    return l2;
+  }
+};
+
+// 迭代
+var mergeTwoLists2 = function(l1, l2) {
+  var preHead = new ListNode();
+  var prev = preHead;
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      prev.next = l1;
+      l1 = l1.next;
+    } else {
+      prev.next = l2;
+      l2 = l2.next;
+    }
+    prev = prev.next;
+  }
+  prev.next = l1 ? l1 : l2;
+
+  return preHead.next;
+};
